cae371f9abc8f2b118d0817695569ccd0c4eb61f
Developer <developer@users.noreply.github.com>
2026-07-04 12:53:13 +0530

    style: exactly match certificates and cards to the clean rounded modern UI of arcade page

--- assets/js/certificates_data.js
+++ assets/js/certificates_data.js
@@ -2,61 +2,51 @@ const certificatesData = [
     {
         "file": "InternshipCertificate.pdf",
         "title": "Internship Certificate",
-        "icon": "fa-solid fa-briefcase",
-        "color": "#4facfe"
+        "icon": "fa-solid fa-briefcase"
     },
     {
         "file": "The_Ultimate_Job_Ready_Data_Science_Course_Certificate.pdf",
         "title": "Data Science Course",
-        "icon": "fa-solid fa-database",
-        "color": "#00f2fe"
+        "icon": "fa-solid fa-database"
     },
     {
         "file": "introduction_to_data_sciecne_certificate.pdf",
         "title": "Intro to Data Science",
-        "icon": "fa-solid fa-chart-pie",
-        "color": "#43e97b"
+        "icon": "fa-solid fa-chart-pie"
     },
     {
         "file": "postma_api_fundamentals_student_expert_badge.pdf",
         "title": "Postman API Badge",
-        "icon": "fa-brands fa-space-awesome",
-        "color": "#ff6b6b"
+        "icon": "fa-brands fa-space-awesome"
     },
     {
         "file": "postma_api_fundamentals_student_expert_certificate.pdf",
         "title": "Postman API Expert",
-        "icon": "fa-solid fa-rocket",
-        "color": "#f093fb"
+        "icon": "fa-solid fa-rocket"
     },
     {
         "file": "problem_solving_basic certificate.pdf",
         "title": "Problem Solving (Basic)",
-        "icon": "fa-solid fa-puzzle-piece",
-        "color": "#f5576c"
+        "icon": "fa-solid fa-puzzle-piece"
     },
     {
         "file": "python_basic certificate.pdf",
         "title": "Python (Basic)",
-        "icon": "fa-brands fa-python",
-        "color": "#30cfd0"
+        "icon": "fa-brands fa-python"
     },
     {
         "file": "tcs_britishairways_virtual_internship_certificate.pdf",
         "title": "TCS British Airways Internship",
-        "icon": "fa-solid fa-plane-departure",
-        "color": "#933515"
+        "icon": "fa-solid fa-plane-departure"
     },
     {
         "file": "tcs_cybersecurity_virtual_internship_certificate.pdf",
         "title": "TCS Cybersecurity Internship",
-        "icon": "fa-solid fa-shield-halved",
-        "color": "#00c6fb"
+        "icon": "fa-solid fa-shield-halved"
     },
     {
         "file": "upskill-campus-internship-completion-certificate.pdf",
         "title": "Upskill Campus Internship",
-        "icon": "fa-solid fa-graduation-cap",
-        "color": "#fa709a"
+        "icon": "fa-solid fa-graduation-cap"
     }
 ];
