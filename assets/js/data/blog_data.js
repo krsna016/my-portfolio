15cdbdaab2ab2a425f0b7beb4db77798e7561f1b
Anurag Pareek <016.krsna@gmail.com>
2026-07-06 13:18:02 +0530

    cms: update blog config (JS) for web-security-1

--- assets/js/data/blog_data.js
+++ assets/js/data/blog_data.js
@@ -1,6 +1,14 @@
 const blogData = {
     "posts": [
         {
+            "id": "web-security-1",
+            "title": "Web Security Fundamentals",
+            "category": "Web Security",
+            "date": "2026-07-06",
+            "summary": "Learn the Fundamentals",
+            "file": "blogs/web-security-1.md"
+        },
+        {
             "id": "test-slug",
             "title": "Test Integration Post",
             "category": "Web Dev",
