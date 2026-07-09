25e0988b2c6c20d2957b664113bc9b376cf40d28
Anurag Pareek <016.krsna@gmail.com>
2026-07-09 12:44:44 +0530

    cms: update blog config (JS) for book-1

--- assets/js/data/blog_data.js
+++ assets/js/data/blog_data.js
@@ -1,6 +1,14 @@
 const blogData = {
     "posts": [
         {
+            "id": "book-1",
+            "title": "The Architecture of Error: Why Smart Minds Make Predictable Mistakes",
+            "category": "Mind",
+            "date": "2026-07-09",
+            "summary": "Read and get it.",
+            "file": "blogs/book-1.md"
+        },
+        {
             "id": "linux-1",
             "title": "Linux Fundamentals - 1",
             "category": "Linux",
@@ -23,6 +31,7 @@ const blogData = {
         "System Design",
         "Web Dev",
         "Web Security",
-        "Linux"
+        "Linux",
+        "Mind"
     ]
 };
