3e8534dea8e8c8a3f3081c6bf465018dd76e3729
Anurag Pareek <016.krsna@gmail.com>
2026-07-11 21:42:58 +0530

    cms: update blog config (JS) after deleting web-security-1

--- assets/js/data/blog_data.js
+++ assets/js/data/blog_data.js
@@ -1,12 +1,20 @@
 const blogData = {
     "posts": [
         {
-            "id": "code-1",
-            "title": "Before Computers, Learn the Language of Information",
-            "category": "CODE",
-            "date": "2026-07-11",
-            "summary": "A deep dive into Morse Code, Braille, Binary Thinking, and the fundamental ideas behind Charles Petzold’s Code.",
-            "file": "blogs/code-1.md"
+            "id": "book-1",
+            "title": "The Architecture of Error: Why Smart Minds Make Predictable Mistakes",
+            "category": "Mind",
+            "date": "2026-07-09",
+            "summary": "Read and get it.",
+            "file": "blogs/book-1.md"
+        },
+        {
+            "id": "linux-1",
+            "title": "Linux Fundamentals - 1",
+            "category": "Linux",
+            "date": "2026-07-07",
+            "summary": "This is Part - 1",
+            "file": "blogs/linux-1.md"
         }
     ],
     "categories": [
@@ -16,7 +24,6 @@ const blogData = {
         "Web Dev",
         "Web Security",
         "Linux",
-        "Mind",
-        "CODE"
+        "Mind"
     ]
 };
