302dadffbdb22b93085b1cd348ee6aa8af1e5f1e
Anurag Pareek <016.krsna@gmail.com>
2026-07-11 09:22:21 +0530

    cms: update blog config (JS) after deleting linux-1

--- assets/js/data/blog_data.js
+++ assets/js/data/blog_data.js
@@ -7,22 +7,6 @@ const blogData = {
             "date": "2026-07-11",
             "summary": "A deep dive into Morse Code, Braille, Binary Thinking, and the fundamental ideas behind Charles Petzold’s Code.",
             "file": "blogs/code-1.md"
-        },
-        {
-            "id": "linux-1",
-            "title": "Linux Fundamentals - 1",
-            "category": "Linux",
-            "date": "2026-07-07",
-            "summary": "This is Part - 1",
-            "file": "blogs/linux-1.md"
-        },
-        {
-            "id": "web-security-1",
-            "title": "Web Security Fundamentals",
-            "category": "Web Security",
-            "date": "2026-07-06",
-            "summary": "Learn the Fundamentals",
-            "file": "blogs/web-security-1.md"
         }
     ],
     "categories": [
