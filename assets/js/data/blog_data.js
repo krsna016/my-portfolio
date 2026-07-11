22a1d907ed6e48e754181d14aa41125fcd04be95
Anurag Pareek <016.krsna@gmail.com>
2026-07-11 23:01:41 +0530

    cms: update blog config (JS) for code-1

--- assets/js/data/blog_data.js
+++ assets/js/data/blog_data.js
@@ -1,6 +1,14 @@
 const blogData = {
     "posts": [
         {
+            "id": "code-1",
+            "title": "Before Computers, Learn the Language of Information",
+            "category": "CODE",
+            "date": "2026-07-11",
+            "summary": "A deep dive into Morse Code, Braille, Binary Thinking, and the fundamental ideas behind Charles Petzold’s Code.",
+            "file": "blogs/code-1.md"
+        },
+        {
             "id": "code-2",
             "title": "Electricity, Batteries & Binary: Understanding Computers from a Flashlight",
             "category": "CODE",
